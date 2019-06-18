ddf8e9b3e8cc17917e254140cc621f8bd79c12ab
NguyenNgocPhuocLong <nguyenngocphuoclong1994@gmail.com>
2019-06-18 20:46:00 +0700

    basic animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,67 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'dart:async';
+import 'package:flutter/animation.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget{
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-        title: 'Form SignUp',
-        theme: ThemeData(
-          primarySwatch: Colors.indigo,
-        ),
-        home: InputBox(),
-      );
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> with SingleTickerProviderStateMixin{
+  Animation animation;
+  AnimationController animationController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController = AnimationController(
+      duration: Duration(seconds: 5),vsync: this
+    );
+
+    animation = Tween(begin: 0.0,end: 500.0).animate(animationController);
+    animation.addStatusListener((status){
+      if(status == AnimationStatus.completed){
+        animationController.reverse();
+      }else if(status == AnimationStatus.dismissed){
+        animationController.forward();
+      }
+    });
+
+    animationController.forward();
   }
-}
-
-class InputBox extends StatefulWidget{
-  @override
-  InputBoxState createState() => InputBoxState();
-}
-
-class InputBoxState extends State<InputBox>{
-  bool loggedIn = false;
-  String _email, _userName, _password;
-
-  final formKey = GlobalKey<FormState>();
-  final mainKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      key: mainKey,
-      appBar: AppBar(title: Text('Form SignUp')),
-      body: Padding(
-          padding: EdgeInsets.all(10.0),
-        child: loggedIn == false?
-        Form(
-          key: formKey,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                autocorrect: false,
-                decoration: InputDecoration(
-                  labelText: "Email"
-                ),
-                validator: (str)=> !str.contains('@')?'Valid Email':null,
-                onSaved: (str) => _email = str,
-              ),
-              TextFormField(
-                autocorrect: false,
-                decoration: InputDecoration(
-                    labelText: "UserName"
-                ),
-                validator: (str)=> str.length < 6?'Valid UserName':null,
-                onSaved: (str) => _userName = str,
-              ),TextFormField(
-                autocorrect: false,
-                obscureText: true,
-                decoration: InputDecoration(
-                    labelText: "Password"
-                ),
-                validator: (str)=> !str.contains('@')?'Valid Password':null,
-                onSaved: (str) => _password = str,
-              ),
-              RaisedButton(
-                color: Colors.blueAccent,
-                child: Text('Sign In'),
-                onPressed: onPresseForm,
-
-              )
-            ],
-          ),
-        ): Center(
-          child: Column(
-            children: <Widget>[
-              Text("Welcome $_userName"),
-              RaisedButton(
-                color: Colors.deepOrange,
-                child: Text("Log out"),
-                onPressed: (){
-                  setState(() {
-                    loggedIn =false;
-                  });
-                },
-              )
-            ],
-          ),
-        ),
-      ),
+    return  LogoAnimation(
+      animation: animation,
     );
   }
 
-  void onPresseForm(){
-    var form = formKey.currentState;
+  @override
+  void dispose(){
+    animationController.dispose();
+    super.dispose();
+  }
+}
 
-    if(form.validate()){
-      form.save();
-      setState(() {
-        loggedIn =true;
-      });
+class LogoAnimation extends AnimatedWidget{
+  LogoAnimation({Key key,Animation animation}) :super(key:key,listenable:animation);
 
-      var snackBar = SnackBar(
-        content:
-        Text('Username: $_userName, Email: $_email, Password: $_password'),
-        duration: Duration(milliseconds: 5000),
-      );
-
-      mainKey.currentState.showSnackBar(snackBar);
-    }
-
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    Animation animation = listenable;
+    return Center(
+      child: Container(
+        height: animation.value,
+        width: animation.value,
+        child: FlutterLogo(),
+      ),
+    );
   }
 }
