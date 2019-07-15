a5e4d9491f5c50376bdd53c31412d953c2d821ad
NguyenNgocPhuocLong <nguyenngocphuoclong1994@gmail.com>
2019-07-15 21:32:33 +0700

    calculator

--- lib/main.dart
+++ lib/main.dart
@@ -4,89 +4,107 @@ import 'package:flutter/material.dart';
 
 void main() =>
     runApp(MaterialApp(
-      title: "Random Squares",
-      home: MyApp(),
+      title: "Gallery",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      home: Calculator(),
     ));
 
-class MyApp extends StatefulWidget {
+class Calculator extends StatefulWidget {
   @override
-  AppState createState() => AppState();
+  CalcState createState() => CalcState();
 }
 
-class AppState extends State<MyApp> {
-  final Random _random = Random();
-  Color color = Colors.amber;
+class CalcState extends State<Calculator> {
+  String inputString = "20 + 30";
 
-  void onTap() {
-    setState(() {
-      color = Color.fromARGB(
-        _random.nextInt(256),
-        _random.nextInt(256),
-        _random.nextInt(256),
-        _random.nextInt(256),
-      );
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return ColorState(
-      color: color,
-      onTap: onTap,
-      child: BoxTree(),
-    );
-  }
-}
-
-class ColorState extends InheritedWidget {
-  ColorState({Key key, this.color, this.onTap, Widget child})
-      : super(key: key, child: child);
-
-  final Color color;
-  final Function onTap;
-
-  @override
-  bool updateShouldNotify(ColorState oldWidget) {
-    // TODO: implement ==
-    print(oldWidget);
-    return color != oldWidget.color;
-  }
-
-  static ColorState of(BuildContext context) {
-    print(context);
-    return context.inheritFromWidgetOfExactType(ColorState);
-  }
-}
-
-class BoxTree extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body: Center(
-        child: Row(
-          children: <Widget>[Box(), Box()],
-        ),
+      appBar: AppBar(
+        title: Text("Calculator"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(16.0),
+              color: Colors.blueGrey.withOpacity(0.85),
+              child: Row(
+                children: <Widget>[
+                  Text(inputString,
+                    style: TextStyle(
+                        color: Colors.black87,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 48.0
+                    ),)
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  makeBtns('C<%/'),
+                  makeBtns('789x'),
+                  makeBtns('456-'),
+                  makeBtns('123+'),
+                  makeBtns('_0.='),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget makeBtns(String row) {
+    List<String> token = row.split("");
+    return Expanded(
+      flex: 1,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: token.map((f) =>
+            CalcButton(
+              keyValue: f == '_' ? "+/-" : e == '<' ? '<=' : e,
+            )).toList(),
       ),
     );
   }
 }
 
-class Box extends StatelessWidget {
+class CalcButton extends StatelessWidget {
+  CalcButton({
+    this.keyValue
+  });
+
+  final String keyValue;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    final colorState = ColorState.of(context);
-    return GestureDetector(
-      onTap: colorState.onTap,
-      onVerticalDragUpdate: (d) => print('drag vertial'),
-      onHorizontalDragUpdate: (d) => print('drag horizon'),
-      child: Container(
-        width: 50.0,
-        height: 50.0,
-        margin: EdgeInsets.only(left: 100.0),
-        color: colorState.color,
+    return Expanded(
+      flex: 1,
+      child: FlatButton(
+        shape: Border.all(
+          color: Colors.grey.withOpacity(0.5),
+          width: 2.0,
+          style: BorderStyle.solid,
+        ),
+        color: Colors.white,
+        child: Text(
+          keyValue,
+          style: TextStyle(
+            fontWeight: FontWeight.normal,
+            fontSize: 36.0,
+            color: Colors.black54,
+            fontStyle: FontStyle.normal,
+          ),
+        ),
       ),
     );
   }
